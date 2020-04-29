b654450905820ed8ca71dc2614d090ba5dd0bc8d
Peter Szalay <peterszalay@users.noreply.github.com>
2020-04-29 22:10:47 +0200

    LuxorTicketTest

---
+++ tests/LuxorTicketTest.php
@@ -0,0 +1,18 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use LuxorPlayer\LuxorTicket;
+
+class LuxorticketrTest extends TestCase
+{
+    public function testCreate()
+    {
+        $luxorTicket = LuxorTicket::create([],[]);
+        $this->assertEquals($luxorTicket->picture, []);
+        $this->assertEquals($luxorTicket->frame, []);
+        
+        $luxorTicket = LuxorTicket::create([25,20,45,38,48,46],[12,2,11,8,19,22,43,36,57,59,70,69,73,72]);
+        $this->assertEquals($luxorTicket->picture, [25,20,45,38,48,46]);
+        $this->assertEquals($luxorTicket->frame, [12,2,11,8,19,22,43,36,57,59,70,69,73,72]);
+        
+    }
+}

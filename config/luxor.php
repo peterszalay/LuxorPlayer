e8ea1c728b3bd5ca28c2a07e825d066850ed7cce
Peter Szalay <peterszalay@users.noreply.github.com>
2020-05-22 22:18:44 +0200

    First plan for autoplayer

--- config/luxor.php
+++ config/luxor.php
@@ -23,8 +23,39 @@ return [
                           'previous_draws' => [11,12,13,14],
                            /* "LEAST_DRAWN","MOST_DRAWN","LEAST_DRAWN_AND_RANDOM","MOST_DRAWN_AND_RANDOM" */
                           'strategies' => ["LEAST_DRAWN","MOST_DRAWN","LEAST_DRAWN_AND_RANDOM","MOST_DRAWN_AND_RANDOM","LEAST_AND_MOST_DRAWN","MOST_LEAST_AND_RANDOM"],
-                          'one_selection' => [20,25,30,35],
+                          'one_selection' => [25,30,35],
                           'two_selections' => ['first' => [5,10,15,20,25], 'second' => [5,10,15,20,25]],
                           'three_selections' => ['first' => [5,10,15,20], 'second' => [5,10,15,20], 'third' => [5,10]],
+                        ],
+        'autoplayer' =>
+                        [
+                            'draws' => 522,
+                            'max_previous_draws' => 104,
+                            'repeat' => 1,
+                            'tickets_per_player' => 50,
+                            'players' => [
+                                            0 => [
+                                                    'name' => 'Sam Random',
+                                                    'strategies' => ["SAME_RANDOM"]
+                                                 ],
+                                            1 => [
+                                                    'name' => 'Reginald Random',
+                                                    'strategies' => ["REGENERATED_RANDOM"]
+                                                 ],
+                                            2 => [
+                                                    'name' => 'Bess Van Jahr',
+                                                    'description' => '', 
+                                                    'previous_draws' => [1,2,3,4,5], 
+                                                    'strategies' => ["LEAST_DRAWN","MOST_DRAWN","LEAST_DRAWN_AND_RANDOM","MOST_DRAWN_AND_RANDOM","LEAST_AND_MOST_DRAWN","MOST_LEAST_AND_RANDOM"],
+                                                    'strategies_played' => 1,
+                                                    'weeks_analyzed' => 52,
+                                                    'repeat' => 10,
+                                                    'min_selection' => 25,
+                                                    'max_selection' => 40,
+                                                    'one_selection' => [25,30,35],
+                                                    'two_selections' => ['first' => [5,10,15,20,25], 'second' => [5,10,15,20,25]],
+                                                    'three_selections' => ['first' => [5,10,15,20], 'second' => [5,10,15,20], 'third' => [5,10]]
+                                                 ]
+                                         ]   
                         ]
         ];
---
+++ src/LuxorPlayer/AutoPlayer.php
@@ -0,0 +1,24 @@
+<?php
+namespace LuxorPlayer;
+
+
+class AutoPlayer {
+    
+    private $players = [];
+    private $results = [];
+    
+    public function __construct(){
+        
+    }
+    
+    public function loadPlayers(){
+        //load players from luxor config file
+        //initialize $results array
+    }
+    
+    public function play(){
+        //loop through the draws and play with each player separately
+        //when a player finishes print out and to file the results with that player
+    }
+
+}

0a104628981bbc7902dfcfbe5850ddde2a15f35a
Peter Szalay <peterszalay@users.noreply.github.com>
2020-06-09 21:49:59 +0200

    new players

--- config/luxor.php
+++ config/luxor.php
@@ -32,11 +32,11 @@ return [
         'auto_player' =>
                         [
                             'draws_played' => 52,
-                            'weeks_analyzed' => 6,
+                            'weeks_analyzed' => 5,
                             'tickets_per_player' => 50,
                             'repeat' => 10,
                             'min_selection' => 25,
-                            'max_selection' => 40,
+                            'max_selection' => 45,
                             'previous_draws' => [2,3,4,5,6,7,9,11,14],
                             'strategies' => ["LEAST_DRAWN","MOST_DRAWN","LEAST_DRAWN_AND_RANDOM","MOST_DRAWN_AND_RANDOM","LEAST_AND_MOST_DRAWN","MOST_LEAST_AND_RANDOM"],
                             'one_selection' => [25,30,35,40],
@@ -97,22 +97,22 @@ return [
                                             8 =>[
                                                     'name' => 'Franz Least',
                                                     'description' => '',
+													'max_selection' => 35,
                                                     'strategies' => ["LEAST_DRAWN","LEAST_AND_MOST_DRAWN","MOST_LEAST_AND_RANDOM"],
                                                     'one_selection' => [25,30,35],
                                                     'two_selections' => ['first' => [25,30], 'second' => [5,10]],
                                                     'three_selections' => ['first' => [5,10], 'second' => [20,25], 'third' => [5,10]],
-                                                    'max_selection' => 35,
                                                     'strategies_played' => 5,
                                                     'order_by' => 'orderByPicturesAndFrames'
                                                 ],
                                             9 =>[
                                                     'name' => 'Elon Most',
                                                     'description' => '',
+													'max_selection' => 35,
                                                     'strategies' => ["MOST_DRAWN","LEAST_AND_MOST_DRAWN","MOST_LEAST_AND_RANDOM"],
                                                     'one_selection' => [25,30,35],
                                                     'two_selections' => ['first' => [5,10], 'second' => [25,30]],
                                                     'three_selections' => ['first' => [20,25], 'second' => [5,10], 'third' => [5,10]],
-                                                    'max_selection' => 35,
                                                     'strategies_played' => 5,
                                                     'order_by' => 'orderByPicturesAndFrames'
                                                 ],
@@ -131,7 +131,7 @@ return [
 		                            		11 =>[
 		                            				'name' => '10 second Tom',
 		                            				'description' => '',
-		                            				'max_selection' => 30,
+		                            				'max_selection' => 35,
 		                            				'previous_draws' => [2,3,4],
 		                            				'strategies' => ["LEAST_DRAWN","MOST_DRAWN","LEAST_AND_MOST_DRAWN","MOST_LEAST_AND_RANDOM"],
 		                            				'one_selection' => [25,30],
@@ -139,7 +139,24 @@ return [
 		                            				'three_selections' => ['first' => [10,15,20,25], 'second' => [5,10,15,20], 'third' => [5,10]],
 		                            				'strategies_played' => 10,
 		                            				'order_by' => 'orderByPicturesAndFrames'
-		                            		],
+												],
+											12 =>[
+		                            				'name' => 'Ken Nutdesaid',
+		                            				'description' => '',
+		                            				'max_selection' => 30,
+		                            				'one_selection' => [25,30],
+		                            				'two_selections' => ['first' => [5,10,15,20,25], 'second' => [5,10,15,20,25]],
+		                            				'three_selections' => ['first' => [10,15,20,25], 'second' => [5,10,15,20], 'third' => [5,10]],
+		                            				'strategies_played' => 25,
+		                            				'order_by' => 'orderByPicturesAndFrames'
+												],
+											13 =>[
+		                            				'name' => 'DoubleMagician',
+		                            				'description' => '',
+		                            				'strategies_played' => 2,
+		                            				'order_by' => 'orderByPicturesAndFrames'
+												],
+											
                                 
                                         ]   
                         ]

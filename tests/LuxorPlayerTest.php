b38d32d09cd58f8ddbcfa3cfe91fc921e8562bf8
Peter Szalay <peterszalay@users.noreply.github.com>
2020-06-14 23:41:35 +0200

    testing ordering
    error returnded by uasort

--- tests/LuxorPlayerTest.php
+++ tests/LuxorPlayerTest.php
@@ -6,6 +6,7 @@ class LuxorPlayerTest extends TestCase
 { 
     protected $luxorPlayer;
     protected $draws;
+    protected $results;
     
     protected function setUp(): void
     {
@@ -13,6 +14,12 @@ class LuxorPlayerTest extends TestCase
         $this->luxorPlayer->init();
         $this->luxorPlayer->setDrawCount(1);
         $this->luxorPlayer->setTicketCount(2);
+        $this->results = ['a' => ['luxor' => 0, 'unique_luxor' => 0, 'unique_frame' => 5, 'unique_picture' => 15, 'frames' => 5, 'pictures' => 20], 
+                          'b' => ['luxor' => 1, 'unique_luxor' => 1, 'unique_frame' => 3, 'unique_picture' => 8, 'frames' => 5, 'pictures' => 10], 
+                          'c' => ['luxor' => 0, 'unique_luxor' => 0, 'unique_frame' => 5, 'unique_picture' => 10, 'frames' => 10, 'pictures' => 15],
+                          'd' => ['luxor' => 0, 'unique_luxor' => 0, 'unique_frame' => 5, 'unique_picture' => 10, 'frames' => 10, 'pictures' => 15],
+                          'e' => ['luxor' => 2, 'unique_luxor' => 1, 'unique_frame' => 3, 'unique_picture' => 7, 'frames' => 5, 'pictures' => 8]
+                         ];
     }
     
     public function testPlayWithRandomNumbersReturnsAssociativeArrayWithCorrectKeys()
@@ -48,6 +55,19 @@ class LuxorPlayerTest extends TestCase
         $this->assertArrayHasKey('first_picture_dates', $result);
         $this->assertArrayHasKey('picture_dates', $result);
         $this->assertArrayHasKey('frame_dates', $result);
+    }
+    
+    public function testOrderByUniquePicturesAndFramesReturnsCorrectOrder()
+    {
+        $reflector = new ReflectionClass(LuxorPlayer::class);
+        $method = $reflector->getMethod('orderByUniquePicturesAndFrames');
+        $method->setAccessible(true);
+        
+        var_dump($reflector->hasMethod('orderByUniquePicturesAndFrames'));
+        
+        uasort($this->results, [$reflector, 'orderByUniquePicturesAndFrames']);
+        
+        $this->assertEquals(implode('',array_keys($this->results)), 'acdbe');
     }
     
 }
